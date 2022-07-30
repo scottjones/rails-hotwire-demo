1b0544f31be087f12d161bf13242293e6dad5614
Scott Jones <scottajones@gmail.com>
2022-07-30 17:16:44 -0400

    Delete messages on cascade when deleting rooms.

---
+++ db/migrate/20220730210944_delete_messages_on_cascade.rb
@@ -0,0 +1,6 @@
+class DeleteMessagesOnCascade < ActiveRecord::Migration[7.0]
+  def change
+    remove_foreign_key :messages, :rooms
+    add_foreign_key :messages, :rooms, on_delete: :cascade
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_30_133102) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_30_210944) do
   create_table "messages", force: :cascade do |t|
     t.integer "room_id", null: false
     t.text "content"
@@ -25,5 +25,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_30_133102) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "rooms", on_delete: :cascade
 end
